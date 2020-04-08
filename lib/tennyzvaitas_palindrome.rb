9012935a10bed780e554e936ef99f25220ce27c7
tennyson <tzvaita@gmail.com>
2020-04-08 15:40:55 +0200

    Finished working and refactored code

--- lib/tennyzvaitas_palindrome.rb
+++ lib/tennyzvaitas_palindrome.rb
@@ -1,6 +1,16 @@
 require "tennyzvaitas_palindrome/version"
 
-module TennyzvaitasPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
